7ad811046e887aabbe156b8be76bd29dae66e7e3
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-04-12 23:49:20 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,51 @@
+import re, asyncio, random
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from pyrogram.types import CallbackQuery
+import os
+
+ajvad = Client(
+    "URUVASHI_FILTER",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
+)
+
+movie1_down_2 = [[                                                 
+                  InlineKeyboardButton("D O W N L O A D", url="https://t.me/c/1314197326/2"),
+                  InlineKeyboardButton("close", callback_data="close")
+                  ]]
+     
+@ajvad.on_callback_query()
+async def callback(bot, msg: CallbackQuery): 
+    if msg.data == "close":
+      await msg.message.delete()       
+                               
+@ajvad.on_message(filters.regex("movie1") & filters.group)
+async def my_handle56814(bot, message):
+    await message.reply_photo( 
+        photo="https://telegra.ph/file/3b77e4cd7c63fef7145ee.jpg",
+        caption="""☞ 𝙼𝙾𝚅𝙸𝙴 :
+☞ 𝚈𝙴𝙰𝚁 :
+☞ 𝚀𝚄𝙰𝙻𝙸𝚃𝚈 : 720p,1080p, 480p
+☞ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : 𝙷𝙸𝙽𝙳𝙸, 𝙼𝙰𝙻𝙰𝚈𝙰, 𝚃𝙰𝙼𝙸, 𝚃𝙴𝙻""",
+        reply_markup=InlineKeyboardMarkup(movie1_down_2)
+    )
+
+movie1_down_1 = [[                                                 
+                  InlineKeyboardButton("D O W N L O A D", url="https://t.me/c/1314197326/2"),
+                  InlineKeyboardButton("close", callback_data="close")
+                  ]]       
+
+@ajvad.on_message(filters.regex("movie2") & filters.group)
+async def my_handle56814(bot, message):
+    await message.reply_photo( 
+        photo="https://telegra.ph/file/f6f4d7cf4452f2c63dd3d.jpg",
+        caption="""☞ 𝙼𝙾𝚅𝙸𝙴 :
+☞ 𝚈𝙴𝙰𝚁 :
+☞ 𝚀𝚄𝙰𝙻𝙸𝚃𝚈 : 720p,1080p, 480p
+☞ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : 𝙷𝙸𝙽𝙳𝙸, 𝙼𝙰𝙻𝙰𝚈𝙰, 𝚃𝙰𝙼𝙸, 𝚃𝙴𝙻""",
+        reply_markup=InlineKeyboardMarkup(movie1_down_1)
+    )
+
+ajvad.run() 
