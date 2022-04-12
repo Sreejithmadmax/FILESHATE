747ab6ba8bdf5a69be8cb4efc264e7e77a92a609
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-04-13 00:08:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/bossstory/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
+  git clone https://github.com/Sreejithmadmax/FILESHATE.git /FILESHATE
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LuciferMoringstar-Robot
+  git clone $UPSTREAM_REPO /FILESHATE
 fi
 cd /LuciferMoringstar-Robot
 pip3 install -U -r requirements.txt
-echo "Starting LuciferMoringstar RoBot...."
+echo "Starting FILESHATE...."
 python3 main.py
