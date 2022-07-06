0e111b4387147410d21ca8b16f4da7308b5785c1
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-06 07:36:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,3 +13,17 @@ import json
 @sree.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
     await message.reply(START_MSG.format(message.from_user.mention))
+
+
+@sree.on_message(filters.regex("Money Heist") & filters.group)
+async def my_handle56814(bot, message):
+    await message.reply_photo( 
+        photo="https://telegra.ph/file/e442a24f233fac96ce83f.jpg",
+        caption="𝙼𝙾𝚅𝙸𝙴 : Money Heist",
+        reply_markup=InlineKeyboardMarkup(movie1_down_2)
+    )
+
+movie1_down_2 = [[                                                 
+                  InlineKeyboardButton("Seasons", callback_data="start"),
+                  InlineKeyboardButton("close", callback_data="close")
+                  ]]
