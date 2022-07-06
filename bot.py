ad863eab3c044018c32b0fe51c838dfa051b7b4f
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-06 07:43:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
 import re, asyncio, random
 import os
 from pyrogram import Client
-
+           
 
 sree = Client(
-    "URUVASHI_FILTER",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"],
-)           
-
-                   
+              "URUVASHI_FILTER"
+              bot_token = os.environ["BOT_TOKEN"],
+              api_id = int(os.environ["API_ID"]),
+              api_hash = os.environ["API_HASH"],
+              )                   
 
 sree.run() 
