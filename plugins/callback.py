e438bf3d45c0d934ce3b4ac3e85798b6d2609b53
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-06 09:15:33 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -2,8 +2,3 @@ from pyrogram import Client as Sree, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-
-@Sree.on_callback_query()
-async def callback(bot, msg: CallbackQuery): 
-    if msg.data == "close":
-      await msg.message.delete()    
