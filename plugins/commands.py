c77a2a5f05396303cf0e5edb97146b57b4022198
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-06 07:54:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -5,7 +5,7 @@ from Script import script
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import re
-import json
+
 
 
 
@@ -14,16 +14,3 @@ import json
 async def start(bot, message):
     await message.reply(START_MSG.format(message.from_user.mention))
 
-
-@sree.on_message(filters.regex("Money Heist") & filters.group)
-async def my_handle56814(bot, message):
-    await message.reply_photo( 
-        photo="https://telegra.ph/file/e442a24f233fac96ce83f.jpg",
-        caption="𝙼𝙾𝚅𝙸𝙴 : Money Heist",
-        reply_markup=InlineKeyboardMarkup(movie1_down_2)
-    )
-
-movie1_down_2 = [[                                                 
-                  InlineKeyboardButton("Seasons", callback_data="start"),
-                  InlineKeyboardButton("close", callback_data="close")
-                  ]]
