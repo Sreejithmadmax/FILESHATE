f825a0f8a946fad37d6a82171ef24637e7225d2c
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-06 08:02:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,7 +2,7 @@ import os
 import random
 import asyncio
 from Script import script
-from pyrogram import Client, filters
+from pyrogram import Client as Sree, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import re
 
