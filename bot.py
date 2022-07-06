7a3274510678d496c31bbdff426eb5d2315f4936
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-06 07:50:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from pyrogram import Client
            
 
 Sree = Client(
+              "NEW BOT SET",
               bot_token = os.environ["BOT_TOKEN"],
               api_id = int(os.environ["API_ID"]),
               api_hash = os.environ["API_HASH"],
