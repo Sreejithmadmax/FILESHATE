2a9a9d1b3436e049d018dd52c3e5ca7d85d00908
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-06 09:37:47 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,23 +1,63 @@
 from pyrogram import Client as Sree, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkupasspass
+from pyrogram.errors import FloodWait, UserNotParticipant
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
 
+@Client.on_callback_query(filters.regex(r"^(start|help|about|close)$"), group=2)
+async def callback_data(bot, message: CallbackQuery):
 
+    query_data = update.data
 
-    elif msg.data == "jeeva":
+    if query_data == "start":
         buttons = [[
-            InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('👥 ᴜᴘᴅᴀᴛᴇs', url='https://t.me/movies_club_2019'),
-            InlineKeyboardButton('ɢʀᴏᴜᴘ 👥', url='https://t.me/UrvashiTheaters')
-            ],[
-            InlineKeyboardButton('🦋 ꜱᴛᴀᴛꜱ ', callback_data='stats'),
-            InlineKeyboardButton(' ᴀʙᴏᴜᴛ 🦋', callback_data='about')
-            ],[
-            InlineKeyboardButton('❌ ᴄʟᴏꜱᴇ ᴛʜᴇ ᴘᴀɢᴇ ❌', callback_data='close_pages')
-        ]]
+        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+    ],[
+        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+    ],[
+        InlineKeyboardButton('Help ⚙', callback_data="help")
+    ]]
+    
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text="🤣",
+        
+        await update.message.edit_caption(
+            text="Hai",
             reply_markup=reply_markup
         )
+
+
+    elif query_data == "help":
+        buttons = [[
+        InlineKeyboardButton('Home ⚡', callback_data='start'),
+        InlineKeyboardButton('About 🚩', callback_data='about')
+    ],[
+        InlineKeyboardButton('Close 🔐', callback_data='close')
+    ]]
+    
+        reply_markup = InlineKeyboardMarkup(buttons)
+        
+        await update.message.edit_caption(
+            text="hai",
+            reply_markup=reply_markup
+        )
+
+
+    elif query_data == "about": 
+        buttons = [[
+        InlineKeyboardButton('Home ⚡', callback_data='start'),
+        InlineKeyboardButton('Close 🔐', callback_data='close')
+    ]]
+        
+        reply_markup = InlineKeyboardMarkup(buttons)
+        
+        await update.message.edit_caption(
+            text="hai",
+            reply_markup=reply_markup
+        )
+
+
+    elif query_data == "help_me":
+        await update.answer("Do", show_alert=True)
+
+    elif query_data == "close":
+        await update.message.delete()   
