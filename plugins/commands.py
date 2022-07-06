0c87ff93043ee380554a4501492fb839e710c17f
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-06 08:00:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -10,7 +10,7 @@ import re
 
 
 
-@sree.on_message(filters.command('start') & filters.private)
+@Sree.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
     await message.reply(START_MSG.format(message.from_user.mention))
 
