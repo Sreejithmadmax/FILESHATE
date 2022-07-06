c4c3a8f8bea4c3eb673138b436a48282492016bc
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-06 09:13:10 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -3,24 +3,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 
-@Sree.on_callback_query(filters.regex('^help$'))
-async def help_cb(c, m):
-    await m.answer()
-    await help(c, m, True)
-
-
-
-@Sree.on_callback_query(filters.regex('^close$'))
-async def close_cb(c, m):
-    await m.message.delete()
-    await m.message.reply_to_message.delete()
-
-@Sree.on_callback_query(filters.regex('^back$'))
-async def back_cb(c, m):
-    await m.answer()
-    await start(c, m, True)
-
-@Sree.on_callback_query(filters.regex('^about$'))
-async def about_cb(c, m):
-    await m.answer()
-    await about(c, m, True)
+@sree.on_callback_query()
+async def callback(bot, msg: CallbackQuery): 
+    if msg.data == "close":
+      await msg.message.delete()    
