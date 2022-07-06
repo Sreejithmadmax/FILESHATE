828b42f1633a79cc2d68f60da109c4759653d9f2
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-06 08:49:48 +0530

    Create callback.py

---
+++ plugins/callback.py
@@ -0,0 +1,26 @@
+from pyrogram import Client as Sree, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+
+@RenamerNs.on_callback_query(filters.regex('^help$'))
+async def help_cb(c, m):
+    await m.answer()
+    await help(c, m, True)
+
+
+
+@Sree.on_callback_query(filters.regex('^close$'))
+async def close_cb(c, m):
+    await m.message.delete()
+    await m.message.reply_to_message.delete()
+
+@Sree.on_callback_query(filters.regex('^back$'))
+async def back_cb(c, m):
+    await m.answer()
+    await start(c, m, True)
+
+@Sree.on_callback_query(filters.regex('^about$'))
+async def about_cb(c, m):
+    await m.answer()
+    await about(c, m, True)
