79d97d7ece0d3a4be7eb5d93a22497f77852858a
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-06 07:25:57 +0530

    Create commands.py

---
+++ plugins/commands.py
@@ -0,0 +1,8 @@
+import os
+import random
+import asyncio
+from Script import script
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import re
+import json
